4fa5ecdfa3d3aa38095eb78ad844b0b9dad100c2
Martha Cryan <martha.cryan@ibm.com>
2020-10-30 16:59:40 -0500

    fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
+curl -sL http://ibm.biz/tjbot-bootstrap | sudo sh -
 sudo apt-get install node npm
 curl https://raw.github.ibm.com/Martha-Cryan/Bugz-B-Gone/camera/finder.js?token=AABYCNEDECOCKY6UJYLGBUK7UXCOK -o finder.js
 npm i tjbot
