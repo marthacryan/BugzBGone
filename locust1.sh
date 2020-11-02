999a22ab89d351b14ffdafcc0571c6befc6ea9fd
Martha Cryan <martha.cryan@ibm.com>
2020-11-02 10:22:17 -0600

    Fix bootstrap script

--- locust1.sh
+++ locust1.sh
@@ -1,2 +1,2 @@
 yes | sudo apt-get install expect
-curl -sL https://raw.githubusercontent.com/marthacryan/BugzBGone/master/ex.sh | sudo sh -
+curl -sL https://raw.githubusercontent.com/marthacryan/BugzBGone/master/ex.sh | sudo expect -
