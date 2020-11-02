88c9bbead9106f456ab3c6902f0c26553467d356
Martha Cryan <martha.cryan@ibm.com>
2020-11-02 10:47:57 -0600

    Add timeout

--- ex.sh
+++ ex.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/expect
 
-set timeout 120
-
 spawn ./tjbootstrap.sh 
 
 expect {[Y/n]} { send "\n" }
 expect {):} { send "\n" }
 expect {[Y/n]} { send "\n" }
-expect {[Y/n]} { send "\n" }
+expect {[Y/n]:} { send "\n" }
 expect {[Y/n]} { send "Y\n" }
 expect {[Y/n]} { send "\n" }
 expect {[Y/n]} { send "Y\n" }
