0aff2359f71960306d2266483c0b47bb812ca2d4
Martha Cryan <martha.cryan@ibm.com>
2020-11-02 10:54:42 -0600

    Fix bootstrap script

--- ex.sh
+++ ex.sh
@@ -6,9 +6,9 @@ spawn ./tjbootstrap.sh
 
 expect {[Y/n]} { send "\n" }
 expect {):} { send "\n" }
-expect {[Y/n]} { send "\n" }
-expect {[Y/n]} { send "\n" }
-expect {[Y/n]} { send "Y\n" }
+expect {[y/N]} { send "\n" }
+expect {[y/N]} { send "\n" }
+expect {[y/N]} { send "Y\n" }
 expect {[Y/n]} { send "\n" }
 expect {[Y/n]} { send "Y\n" }
 expect {[Y/n]} { send "\n" }
