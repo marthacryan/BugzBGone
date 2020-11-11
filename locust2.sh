a4b7bcde5e862853b3355c8ffce2e9740e62189f
Martha Cryan <martha.cryan@ibm.com>
2020-11-11 00:18:12 -0600

    Update locust2.sh

--- locust2.sh
+++ locust2.sh
@@ -1,3 +1,7 @@
+curl https://raw.githubusercontent.com/marthacryan/BugzBGone/master/finder.js -o finder.js
+curl https://raw.githubusercontent.com/marthacryan/BugzBGone/master/package.json -o package.json
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install 15
