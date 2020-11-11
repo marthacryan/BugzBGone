c13bf771c338b2a6d9b0c68465a76c4e5b99cc9b
Martha Cryan <martha.cryan@ibm.com>
2020-11-10 19:51:24 -0600

    Update locust1.sh

--- locust1.sh
+++ locust1.sh
@@ -104,6 +104,11 @@ cd ..
 curl https://raw.githubusercontent.com/marthacryan/BugzBGone/master/finder.js -o finder.js
 curl https://raw.githubusercontent.com/marthacryan/BugzBGone/master/package.json -o package.json
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install 15
+
 #----remove outdated apt packages
 echo ""
 echo "Removing unused software packages [apt-get autoremove]"
