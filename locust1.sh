f389d767fbc8a7dd640c7820f9a09349b2a47714
Martha Cryan <martha.cryan@ibm.com>
2020-11-11 00:17:40 -0600

    Update locust1.sh

--- locust1.sh
+++ locust1.sh
@@ -101,13 +101,6 @@ tar -xJf node-v10.16.1-linux-armv6l.tar.xz
 cd node-v10.16.1-linux-armv6l/
 sudo cp -R * /usr/local/
 cd ..
-curl https://raw.githubusercontent.com/marthacryan/BugzBGone/master/finder.js -o finder.js
-curl https://raw.githubusercontent.com/marthacryan/BugzBGone/master/package.json -o package.json
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install 15
 
 #----remove outdated apt packages
 echo ""
